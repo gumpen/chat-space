03d368301bd828d5e6a9185a678a156051f0e1de
gumpen <shuhei1993727@gmail.com>
2018-07-19 16:04:46 +0900

    Add groups/new action

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   resources :users, only: [:edit, :update]
+  resources :groups do
+    resources :messages
+  end
 end
