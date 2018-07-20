6df17c7ce0774668d545af02f5a99c90c709127b
gumpen <shuhei1993727@gmail.com>
2018-07-20 14:44:38 +0900

    Add index action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,16 @@
 class MessagesController < ApplicationController
+  before_action :set_group
+
   def index
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
+  end
+
+  def create
+  end
+
+  private
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
