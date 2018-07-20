3e36bd06db5b3e7580b0b0f887b9819617be52a9
gumpen <shuhei1993727@gmail.com>
2018-07-20 15:31:25 +0900

    Add validates

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,11 @@ class Message < ApplicationRecord
 
   belongs_to :user
   belongs_to :group
+
+  validates :body_or_image, presence: true
+
+
+  def body_or_image
+    body.presence or image.presence
+  end
 end
