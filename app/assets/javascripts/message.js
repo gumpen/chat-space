afc8cc6d88409a6876dfc47dbfa4918592f64d38
gumpen <shuhei1993727@gmail.com>
2018-07-24 00:24:58 +0900

    Reflect  asynchronous processing to view

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,15 +1,53 @@
 $(function(){
-  $("#message_submit").on("submit",function(e){
+  function buildHTML(message){
+    var html =
+    `
+    <div class="main__content__message">
+      <div class="main__content__message__upper">
+        <span class="main__content__message__upper__name">
+          ${message.user_name}
+        </span>
+        <span class="main__content__message__upper__datetime">
+          ${message.created_at}
+        </span>
+      </div>
+        <div class="main__content__message__text">
+          ${message.body}
+        </div>
+      ${ message.image? `
+              <img src = '${ message.image }', class='main__content__message__image'>` :''}
+    </div>
+    `;
+    return html;
+  };
+
+  function scroll(){
+    $('.main__content').animate({scrollTop: $('.main__content')[0].scrollHeight},'fast')
+  }
+
+  $("#new_message").on("submit",function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var href = window.location.href;
+    var url = $(this).attr('action');
     $.ajax({
-      url: href,
+      url: url,
       type: 'POST',
       data: formData,
       dataType: 'json',
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      debugger;
+      $('.main__content').append(html);
+      $('.main__footer__formarea__submit').prop('disabled', false);
+      scroll()
+      $('#new_message')[0].reset();
+    })
+    .fail(function(){
+      alert('送信に失敗しました')
+    })
   })
+})
 
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,27 +8,27 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
-    respond_to do |format|
-      format.html do
-        if @message.save
+    if @message.save
+      respond_to do |format|
+        format.html do
           redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
-        else
-          @messages = @group.messages.includes(:user)
-          flash.now[:alert] = 'メッセージを入力してください。'
-          render :index
         end
+        format.json
       end
-      format.json
+    else
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
     end
   end
+end
 
-  private
+private
 
-  def message_params
-    params.require(:message).permit(:body, :image).merge(user_id: current_user.id)
-  end
+def message_params
+  params.require(:message).permit(:body, :image).merge(user_id: current_user.id)
+end
 
-  def set_group
-    @group = Group.find(params[:group_id])
-  end
+def set_group
+  @group = Group.find(params[:group_id])
 end
