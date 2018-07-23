83b3c613bd2ae3abce6b0465d3e61d3953f6d20c
gumpen <shuhei1993727@gmail.com>
2018-07-23 21:46:18 +0900

    Separate html and json process with controller

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,13 +3,13 @@ $(function(){
     e.preventDefault();
     var formData = new FormData(this);
     var href = window.location.href;
+    $.ajax({
+      url: href,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   })
-  $.ajax({
-    url: href,
-    type: 'POST',
-    data: formData,
-    dataType: 'json',
-    processData: false,
-    contentType: false
-  })
-})
+
