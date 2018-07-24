a2b516c018fffb7671ed0dfbe6e5982c2103824c
gumpen <shuhei1993727@gmail.com>
2018-07-24 15:20:45 +0900

    Set up routing and index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(20)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   def edit
   end
 
@@ -9,7 +17,6 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-
 
   private
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update,:index]
   resources :groups do
     resources :messages
   end
