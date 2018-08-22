0e0bdef1eb3e6f44ce2cf539fa727b6b5b87751f
gumpen <shuhei1993727@gmail.com>
2018-08-22 15:20:25 +0900

    Delete debug

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,7 +13,6 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'cs-upload-images'
-  byebug
   if Rails.env.production?
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/cs-upload-images'
   end
